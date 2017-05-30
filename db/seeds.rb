d58d3882f1fce1a3ef0a833ee7cebac17528f1c4
henry2992 <henry2992@hotmail.com>
2017-05-29 22:23:31 -0500

    Add Patients to Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 c = Clinic.create!(name:'Clinica de Test', status: true)
 u = Doctor.create!(email:'henry2992@hotmail.com', password:'test1234', password_confirmation:'test1234', first_name: "Henry", last_name: "Remache", clinic: c)
+
+# Pacientes
+(0..50).each_with_index do |number, index|
+	p = Patient.create!(email: 'paciente' + index.to_s + '@email.com' , password:'test1234', password_confirmation:'test1234', first_name: "Paciente", last_name: "Numero " + index.to_s)
+	p.doctor = u
+	p.save!
+end

eee63bc8c963884fc08c208f6919ed2835ef676e
Johnmer Bencomo <bjohnmer@gmail.com>
2017-09-07 11:10:33 -0400

    Historia Clínica

--- app/controllers/docs/histories_controller.rb
+++ app/controllers/docs/histories_controller.rb
@@ -5,11 +5,11 @@ class Docs::HistoriesController < Docs::DoctorsController
   def update
     respond_to do |format|
       if @patient.history.update(history_params)
+        Answer.where(history: @patient.history).destroy_all.to_json
         params[:history][:questions].each do |a,i|
-          ChoiceQuestion.where(choice: Choice.find(i.to_i), question: Question.find(a.to_i)).destroy_all
-          cq = ChoiceQuestion.create! choice: Choice.find(i.to_i), question: Question.find(a.to_i)
-          raise ChoiceQuestion.where(choice: Choice.find(i.to_i), question: Question.find(a.to_i)).count.to_json
-          cq.answers.create! history: @patient.history, choice: Choice.find(i.to_i)
+          # ChoiceQuestion.where(choice: Choice.find(i.to_i), question: Question.find(a.to_i)).destroy_all
+          # cq = ChoiceQuestion.create! choice: Choice.find(i.to_i), question: Question.find(a.to_i)
+          Answer.create! choice_question: ChoiceQuestion.where(choice: i.to_i, question_id: a.to_i).take, history: @patient.history, choice: Choice.find(i.to_i)
         end
         format.html { redirect_to docs_patient_path(@patient), notice: 'La historia fue actualizada exitosamente.' }
       else
